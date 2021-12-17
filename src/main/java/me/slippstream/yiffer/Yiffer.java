7c298e37555f620863c3a50ae5026d361845d513
Nick <nick.zimanski@gmail.com>
2021-12-17 01:48:00 -0500

    Initial commit

---
+++ src/main/java/me/slippstream/yiffer/Yiffer.java
@@ -0,0 +1,17 @@
+package me.slippstream.yiffer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Yiffer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
